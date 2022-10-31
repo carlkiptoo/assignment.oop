95250273883d8930e610bc8feb41d0b3f74a1eb9
carlkiptoo <86962116+carlkiptoo@users.noreply.github.com>
2022-10-31 08:58:21 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
-public class Main {
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class NumberGuessingWithACounter extends JFrame
+        implements ActionListener {
+
+    private JTextField guessField;
+    private JLabel guessLabel, resultLabel;
+    private JButton guessButton;
+    private int number, counter;
+
+    public NumberGuessingWithACounter() {
+        super("Number Guessing Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+        guessLabel = new JLabel("Enter your guess: ");
+        add(guessLabel);
+        guessField = new JTextField(5);
+        add(guessField);
+        guessButton = new JButton("Guess");
+        add(guessButton);
+        resultLabel = new JLabel("");
+        add(resultLabel);
+        guessButton.addActionListener(this);
+        setSize(400, 90);
+        setVisible(true);
+        number = (int) (Math.random() * 100) + 1;
+        counter = 0;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        int guess = Integer.parseInt(guessField.getText());
+        counter++;
+        if (guess == number) {
+            resultLabel.setText("Correct! You guessed it in "
+                    + counter + " tries.");
+        } else if (guess < number) {
+            resultLabel.setText("Your guess is too low, guesses made: "
+                    + counter);
+        } else if (guess > number) {
+            resultLabel.setText("Your guess is too high, guesses made: "
+                    + counter);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new NumberGuessingWithACounter();
     }
 }
